f18371f3a0cdfbda73a7a3fb0ce2213eb91146b7
Brian Swetland <swetland@google.com>
2011-02-02 15:31:45 -0800

    initial checkin of build for TI Panda Board
    Change-Id: I770676d83ffaaea7b57a297a2c74c8b4ae9949ea

---
+++ device.mk
@@ -0,0 +1,14 @@
+
+DEVICE_PACKAGE_OVERLAYS := 
+
+PRODUCT_COPY_FILES := \
+	device/ti/panda/init.board.rc:root/init.omap4pandaboard.rc
+
+PRODUCT_PACKAGES := 
+
+PRODUCT_PROPERTY_OVERRIDES :=
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
---
+++ panda.mk
@@ -0,0 +1,16 @@
+
+
+PRODUCT_PACKAGES :=
+
+PRODUCT_COPY_FILES :=
+
+$(call inherit-product, device/ti/panda/device.mk)
+
+PRODUCT_NAME := panda
+PRODUCT_DEVICE := panda
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := OMAP44x0 Panda Board
+
+PRODUCT_LOCALES += en_US
+
+PRODUCT_PROPERTY_OVERRIDES +=
