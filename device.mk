7b7601b4d00fa921624135808f471b215a5d7817
Brian Swetland <swetland@google.com>
2011-02-02 16:05:30 -0800

    HC and GB have different strategies for ro.hardware... fix...
    Change-Id: If8efa2c63c4f594c90a2817322647e16ea411f60

--- device.mk
+++ device.mk
@@ -2,7 +2,8 @@
 DEVICE_PACKAGE_OVERLAYS := 
 
 PRODUCT_COPY_FILES := \
-	device/ti/panda/init.board.rc:root/init.omap4pandaboard.rc
+	device/ti/panda/init.board.rc:root/init.omap4.rc
+# TODO: in HC change to init.omap4pandaboard.rc
 
 PRODUCT_PACKAGES := 
 
