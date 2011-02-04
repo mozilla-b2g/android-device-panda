e50cd463e3eb1890de8b1329f9a4331977f0c4cc
Dima Zavin <dima@android.com>
2011-02-04 15:11:13 -0800

    rename init.board.rc to init.omap4pandaboard.rc
    Change-Id: Ifd3c3fbb610f879efc69411309d5712178d5d195
    Signed-off-by: Dima Zavin <dima@android.com>

--- device.mk
+++ device.mk
@@ -2,8 +2,7 @@
 DEVICE_PACKAGE_OVERLAYS := 
 
 PRODUCT_COPY_FILES := \
-	device/ti/panda/init.board.rc:root/init.omap4.rc
-# TODO: in HC change to init.omap4pandaboard.rc
+	device/ti/panda/init.omap4pandaboard.rc:root/init.omap4pandaboard.rc
 
 PRODUCT_PACKAGES := 
 
