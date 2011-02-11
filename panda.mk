7ae50a9cf7a8331f6bd630cf4b85e7b229ff3eb1
Dima Zavin <dima@android.com>
2011-02-11 15:21:57 -0800

    panda: inherit from full_base too
    Change-Id: I6b8cd1978c46cc8eb582665de2ba2e473535fa00
    Signed-off-by: Dima Zavin <dima@android.com>

--- panda.mk
+++ panda.mk
@@ -1,16 +1,15 @@
-
-
 PRODUCT_PACKAGES :=
 
 PRODUCT_COPY_FILES :=
 
+PRODUCT_LOCALES := en_US
+
+PRODUCT_PROPERTY_OVERRIDES :=
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/ti/panda/device.mk)
 
 PRODUCT_NAME := panda
 PRODUCT_DEVICE := panda
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := OMAP44x0 Panda Board
-
-PRODUCT_LOCALES += en_US
-
-PRODUCT_PROPERTY_OVERRIDES +=
