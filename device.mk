8e2358ff2eec73a0bf703dc821f585928ca5a0a7
Erik Gilling <konkers@android.com>
2011-03-14 18:19:32 -0700

    add miscellaneos missing configuration to make panda usable
    Change-Id: Ie8bcab72cf2d1af070d6df26b47b8a5a12e0e27a

--- device.mk
+++ device.mk
@@ -1,14 +1,15 @@
 
-DEVICE_PACKAGE_OVERLAYS := 
+DEVICE_PACKAGE_OVERLAYS := device/ti/panda/overlay
 
 PRODUCT_COPY_FILES := \
 	device/ti/panda/init.omap4pandaboard.rc:root/init.omap4pandaboard.rc
 
 PRODUCT_PACKAGES := 
 
-PRODUCT_PROPERTY_OVERRIDES :=
+PRODUCT_PROPERTY_OVERRIDES := \
+	dalvik.vm.heapsize=48m
 
-PRODUCT_CHARACTERISTICS := nosdcard
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
--- panda.mk
+++ panda.mk
@@ -1,12 +1,14 @@
-PRODUCT_PACKAGES :=
+PRODUCT_PACKAGES := \
+	librs_jni
 
-PRODUCT_COPY_FILES :=
+PRODUCT_COPY_FILES := \
+	device/ti/panda/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_LOCALES := en_US
 
 PRODUCT_PROPERTY_OVERRIDES :=
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 $(call inherit-product, device/ti/panda/device.mk)
 
 PRODUCT_NAME := panda
