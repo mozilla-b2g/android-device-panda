f91ebfd098e063e7f0b7209affbe0316a4b6471a
Jean-Baptiste Queru <jbq@google.com>
2011-07-14 07:56:04 -0700

    Create directory for dhcpcd data, set DNS properties
    Change-Id: Iac67e4c0f2d7ee1795bb9b2b2dacf19713fe3b80

--- full_panda.mk
+++ full_panda.mk
@@ -29,6 +29,10 @@ PRODUCT_PACKAGES += \
         VisualizationWallpapers \
         librs_jni
 
+PRODUCT_PROPERTY_OVERRIDES := \
+        net.dns1=8.8.8.8 \
+        net.dns2=8.8.4.4
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/ti/panda/device.mk)
