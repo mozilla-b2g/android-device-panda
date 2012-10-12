4555adb95018712eaacc29515d591dc32c61b90d
Malini Das <mdas@mozilla.com>
2012-10-12 15:36:08 -0400

    remove unnecessary logging

--- sut.sh
+++ sut.sh
@@ -1,10 +1,7 @@
 #!/system/bin/sh
 #set up eth0 networking
 psoutput=`ps sut`
-echo $psoutput >> /data/local/netcf.log
 if [[ $psoutput != *"sutagent"* ]] ; then
-  echo startingsut >> /data/local/netcf.log
   export NSPR_LOG_MODULES="NegatusLOG:5, timestamp" && LD_LIBRARY_PATH="/data/local:/data/local/agent:/system/b2g:$LD_LIBRARY_PATH"
   sutagent &>> /data/local/sut.log
-  echo started >> /data/local/netcf.log
 fi
